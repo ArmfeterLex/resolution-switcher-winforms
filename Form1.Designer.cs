9841339f315645764f772388607505d119eeb370
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-01 14:00:03 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp9x
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 244);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Определить размер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(171, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Изменить разрешение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Дисплей";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp9x
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Screen.PrimaryScreen.Bounds.Width + "x" + Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+        [DllImport("user32.dll")]
+        public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct DEVMODE
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public short dmOrientation;
+            public short dmPaperSize;
+            public short dmPaperLength;
+            public short dmPaperWidth;
+            public short dmScale;
+            public short dmCopies;
+            public short dmDefaultSource;
+            public short dmPrintQuality;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public short dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+        public const int ENUM_CURRENT_SETTINGS = -1;
+        public const int CDS_UPDATEREGISTRY = 1;
+        public const int CDS_TEST = 2;
+        public const int DISP_CHANGE_SUCCESSFUL = 0;
+        public const int DISP_CHANGE_RESTART = 1;
+        public const int DISP_CHANGE_FAILED = -1;
+
+        private int clickCount = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int iWidth, iHeight;
+            switch (clickCount % 3)
+            {
+                case 0:
+                    iWidth = 640;
+                    iHeight = 480;
+                    break;
+                case 1:
+                    iWidth = 800;
+                    iHeight = 600;
+                    break;
+                case 2:
+                default:
+                    iWidth = 1920;
+                    iHeight = 1080;
+                    break;
+            }
+
+            DEVMODE dm = new DEVMODE();
+            dm.dmSize = (short)Marshal.SizeOf(dm);
+            if (EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref dm) != 0)
+            {
+                dm.dmPelsWidth = iWidth;
+                dm.dmPelsHeight = iHeight;
+                dm.dmFields = 0x00080000 | 0x00100000;
+
+                int iRet = ChangeDisplaySettings(ref dm, CDS_TEST);
+                if (iRet == DISP_CHANGE_FAILED)
+                {
+                    MessageBox.Show("Не получается поменять разрешение");
+                }
+                else
+                {
+                    iRet = ChangeDisplaySettings(ref dm, CDS_UPDATEREGISTRY);
+                    switch (iRet)
+                    {
+                        case DISP_CHANGE_SUCCESSFUL:
+                            break;
+                        case DISP_CHANGE_RESTART:
+                            MessageBox.Show("Нужно перезагрузить компьютер");
+                            break;
+                        default:
+                            MessageBox.Show("Ошибка при изменении разрешения");
+                            break;
+                    }
+                }
+            }
+            clickCount++;
+        }
+    }
+}
